d39aff45284d9e65eceb3348ff6b5b510b68f42a
Jim Hessin <jhessin@gmail.com>
2023-01-24 09:36:44 -0800

    Added bootstrap styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,35 @@
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/settings">Settings</a>
-	<a href="/blog/hello-world">Hello World</a>
+<script lang="ts">
+	import { page } from '$app/stores';
+	import './styles.css';
+</script>
+
+<nav class="nav nav-tabs nav-fill">
+	<a
+		href="/"
+		class="nav-link"
+		class:active={$page.url.pathname === '/'}
+		aria-current={$page.url.pathname === '/' ? 'page' : undefined}>Home</a
+	>
+	<a
+		href="/about"
+		class="nav-link"
+		class:active={$page.url.pathname === '/about'}
+		aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>About</a
+	>
+	<a
+		href="/settings"
+		class="nav-link"
+		class:active={$page.url.pathname === '/settings'}
+		aria-current={$page.url.pathname === '/settings' ? 'page' : undefined}>Settings</a
+	>
+	<a
+		href="/blog/hello-world"
+		class="nav-link"
+		class:active={$page.url.pathname === '/blog/hello-world'}
+		aria-current={$page.url.pathname === '/blog/hello-world' ? 'page' : undefined}>Hello World</a
+	>
 </nav>
 
-<slot />
+<div class="container">
+	<slot />
+</div>
