a4ce03042895199cea8d290c0fae9aa2f1c6cc6f
Jim Hessin <jhessin@gmail.com>
2023-02-07 12:45:59 -0700

    A working Sign-in/sign-out flow

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,21 @@
+import {
+	FIREBASE_API_KEY,
+	FIREBASE_APP_ID,
+	FIREBASE_MEASUREMENT_ID,
+	FIREBASE_MESSAGING_SENDER_ID,
+} from '$env/static/private';
+import type { PageServerLoad } from './$types';
+
+const firebaseConfig = {
+	apiKey: FIREBASE_API_KEY,
+	authDomain: 'go-fer.firebaseapp.com',
+	projectId: 'go-fer',
+	storageBucket: 'go-fer.appspot.com',
+	messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+	appId: FIREBASE_APP_ID,
+	measurementId: FIREBASE_MEASUREMENT_ID,
+};
+
+export const load: PageServerLoad = (event) => {
+	return { firebaseConfig };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,44 @@
+<script lang="ts">
+	import { initializeApp } from 'firebase/app';
+	import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, type User } from 'firebase/auth';
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+
+	const app = initializeApp(data.firebaseConfig, 'Go-fer');
+	const auth = getAuth(app);
+
+	export let user: User | null;
+
+	auth.onAuthStateChanged((newUser) => (user = newUser));
+	async function login() {
+		try {
+			await signInWithPopup(auth, new GoogleAuthProvider());
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	async function logout() {
+		try {
+			await signOut(auth);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+</script>
+
 <h1 class="display-3">Welcome to Go-fer!</h1>
 
 <p class="lead">
 	Go-fer is a contruction material delivery app for when you need that one thing right now...
 </p>
+
+{#if !user}
+	<button on:click={() => login()}>Sign In</button>
+{:else}
+	<p>Welcome {user.displayName}!</p>
+	<button on:click={() => logout()}>Sign Out</button>
+{/if}
 
 <p>
 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem quae fugiat similique nam
